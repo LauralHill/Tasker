5e695b2734725df4978a96cea5125fb9a25493d1
LauralHill <37973203+LauralHill@users.noreply.github.com>
2018-04-01 01:36:39 -0700

    Add files via upload
    More Hotspot tasks and profiles.

---
+++ hotspot.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import subprocess
+import re
+try: #python3
+    from urllib.request import urlopen
+except: #python2
+    from urllib2 import urlopen
+
+
+#import requests
+
+# Store Mac address of all nodes here
+saved = {
+    'c4:17:fe:67:8f:19': 'My laptop',
+    'ec:88:92:79:8e:9e': 'Old phone',
+    '68:37:e9:7e:9b:c6': 'Kindle',
+}
+
+# Set wireless interface using ifconfig
+interface = "wlan0"
+
+mac_regex = re.compile(r'([a-zA-Z0-9]{2}:){5}[a-zA-Z0-9]{2}')
+
+
+def parse_arp():
+    arp_out = subprocess.check_output(f'arp -i {interface}', shell=True).decode('utf-8')
+    if 'no match found' in arp_out:
+        return None
+
+    result = []
+    for lines in arp_out.strip().split('\n'):
+        line = lines.split()
+        if interface in line and '(incomplete)' not in line:
+            for element in line:
+                # If its a mac addr
+                if mac_regex.match(element):
+                    result.append((line[1], element))
+    return result
+
+
+
+def print_device(device, num=0):
+    device_name = saved[device[1]] if device[1] in saved else 'unrecognised !!'
+
+    print('name: ',device_name,'\nMac:', device[1], '\nIP: ',device[0],',')
+
+if __name__ == '__main__':
+    print('Retrieving connected devices ..\n')
+
+    devices = parse_arp()
+
+    if not devices:
+        print('No devices found!')
+
+    else:
+        try:
+            num = 0
+            for device in devices:
+                num += 1
+                print_device(device, num)
+        except KeyboardInterrupt as e:
+            num = 0
+            for device in devices:
+                num += 1
+                print_device(device, num,",")
